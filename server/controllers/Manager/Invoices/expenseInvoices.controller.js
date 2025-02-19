7f4396fc56c8ced14c1b8d7f8196219724952d47
hydrazin60 <sureshkhdk45@gmail.com>
2025-02-19 17:32:35 +0545

    create UpdateExpenseInvoice , getAllExpenseInvoices , getAllExpenseInvoicesOnBranch , getOneExpenseInvoice , deleteExpenseInvoice API

--- server/controllers/Manager/Invoices/expenseInvoices.controller.js
+++ server/controllers/Manager/Invoices/expenseInvoices.controller.js
@@ -1,6 +1,7 @@
 import ExpenseInvoice from "../../../models/invoice/expense_Invoice/expense.model.js";
 import Worker from "../../../models/user/worker/worker.models.js";
 import Branch from "../../../models/Branch/Branch.model.js";
+import mongoose from "mongoose";
 
 export const CreateExpenseInvoice = async (req, res) => {
   try {
@@ -91,6 +92,325 @@ export const CreateExpenseInvoice = async (req, res) => {
       success: false,
       error: true,
       message: `Error in isAuthenticated middleware: ${error}`,
+    });
+  }
+};
+
+export const UpdateExpenseInvoice = async (req, res) => {
+  try {
+    const AutherId = req.staffId;
+    const ExpenseInvoiceId = req.params.expenseInvoiceId;
+    const {
+      Price,
+      PaymentMethod,
+      PaidName,
+      typeOfExpense,
+      tax,
+      description,
+      RefNumber,
+      messageOnStatement,
+    } = req.body;
+
+    console.log(req.body);
+    console.log(AutherId);
+    if (!mongoose.Types.ObjectId.isValid(ExpenseInvoiceId)) {
+      return res.status(400).json({
+        success: false,
+        error: true,
+        message: "Invalid ExpenseInvoiceId",
+      });
+    }
+    if (!mongoose.Types.ObjectId.isValid(AutherId)) {
+      return res.status(400).json({
+        success: false,
+        error: true,
+        message: "Invalid AutherId",
+      });
+    }
+    const AutherData = await Worker.findById(AutherId);
+    if (!AutherData) {
+      return res.status(404).json({
+        success: false,
+        error: true,
+        message: "Author not found",
+      });
+    }
+    if (AutherData.role !== "Manager" && AutherData.role !== "Admin") {
+      return res.status(403).json({
+        success: false,
+        error: true,
+        message: "You are not authorized to update this expense invoice",
+      });
+    }
+    const BranchData = await AutherData.BranchId;
+
+    if (AutherData.role !== "Admin") {
+      if (AutherData._id.toString() !== BranchData.BranchStaff.toString()) {
+        return res.status(403).json({
+          success: false,
+          error: true,
+          message: "You are not authorized to update this expense invoice",
+        });
+      }
+    }
+
+    const ExpenseInvoiceData = await ExpenseInvoice.findById(ExpenseInvoiceId);
+    if (!ExpenseInvoiceData) {
+      return res.status(404).json({
+        success: false,
+        error: true,
+        message: "Expense invoice not found",
+      });
+    }
+
+    if (Price !== undefined) ExpenseInvoiceData.Price = Price;
+    if (PaymentMethod !== undefined)
+      ExpenseInvoiceData.PaymentMethod = PaymentMethod;
+    if (PaidName !== undefined) ExpenseInvoiceData.PaidName = PaidName;
+    if (typeOfExpense !== undefined)
+      ExpenseInvoiceData.typeOfExpense = typeOfExpense;
+    if (tax !== undefined) ExpenseInvoiceData.tax = tax;
+    if (description !== undefined) ExpenseInvoiceData.description = description;
+    if (RefNumber !== undefined) ExpenseInvoiceData.RefNumber = RefNumber;
+    if (messageOnStatement !== undefined)
+      ExpenseInvoiceData.messageOnStatement = messageOnStatement;
+
+    await ExpenseInvoiceData.save();
+    const populateData = await ExpenseInvoice.findById(ExpenseInvoiceId)
+      .populate("BranchId")
+      .populate("expenseInvoiceCreatedBy");
+
+    return res.status(200).json({
+      success: true,
+      error: false,
+      message: "Expense invoice updated successfully",
+      data: populateData,
+    });
+  } catch (error) {
+    console.log(` error in updating expense invoice: ${error}`);
+    return res.status(500).json({
+      success: false,
+      error: true,
+      message: `Error in updating expense invoice: ${error}`,
+    });
+  }
+};
+//  only Admin
+export const getAllExpenseInvoices = async (req, res) => {
+  try {
+    const AutherId = req.staffId;
+    if (!mongoose.Types.ObjectId.isValid(AutherId)) {
+      return res.status(400).json({
+        success: false,
+        error: true,
+        message: "Invalid AutherId",
+      });
+    }
+    const AutherData = await Worker.findById(AutherId);
+    if (!AutherData) {
+      return res.status(404).json({
+        success: false,
+        error: true,
+        message: "Author not found",
+      });
+    }
+    if (AutherData.role !== "Admin") {
+      return res.status(403).json({
+        success: false,
+        error: true,
+        message: "You are not authorized to get this expense invoice",
+      });
+    }
+    const AllExpenseInvoices = await ExpenseInvoice.find()
+      .populate("BranchId")
+      .populate("expenseInvoiceCreatedBy");
+
+    return res.status(200).json({
+      success: true,
+      error: false,
+      message: `All expense invoice fetched successfully`,
+      data: AllExpenseInvoices,
+    });
+  } catch (error) {
+    console.log(` error in getting all expense invoice: ${error}`);
+    return res.status(500).json({
+      success: false,
+      error: true,
+      message: `Error in getting all expense invoice: ${error}`,
+    });
+  }
+};
+
+export const getAllExpenseInvoicesOnBranch = async (req, res) => {
+  try {
+    const AutherId = req.staffId;
+    const branchId = req.params.branchId;
+    if (!mongoose.Types.ObjectId.isValid(AutherId)) {
+      return res.status(400).json({
+        success: false,
+        error: true,
+        message: "Invalid AutherId",
+      });
+    }
+    if (!mongoose.Types.ObjectId.isValid(branchId)) {
+      return res.status(400).json({
+        success: false,
+        error: true,
+        message: "Invalid branchId",
+      });
+    }
+
+    const AutherData = await Worker.findById(AutherId);
+    if (!AutherData) {
+      return res.status(404).json({
+        success: false,
+        error: true,
+        message: "Author not found",
+      });
+    }
+
+    if (AutherData.role !== "Admin" && AutherData.role !== "Manager") {
+      return res.status(403).json({
+        success: false,
+        error: true,
+        message: "You are not authorized to get this expense invoice",
+      });
+    }
+
+    if (AutherData.role !== "Admin") {
+      if (AutherData._id.toString() !== AutherData.BranchId.toString()) {
+        return res.status(403).json({
+          success: false,
+          error: true,
+          message: "You are not authorized to get this expense invoice",
+        });
+      }
+    }
+    const AllExpenseInvoices = await ExpenseInvoice.find({ BranchId: branchId })
+      .populate("BranchId")
+      .populate("expenseInvoiceCreatedBy");
+
+    return res.status(200).json({
+      success: true,
+      error: false,
+      message: `All expense invoice fetched successfully`,
+      data: AllExpenseInvoices,
+    });
+  } catch (error) {
+    console.log(` error in getting all expense invoice: ${error}`);
+    return res.status(500).json({
+      success: false,
+      error: true,
+      message: `Error in getting all expense invoice: ${error}`,
+    });
+  }
+};
+
+export const getOneExpenseInvoice = async (req, res) => {
+  try {
+    const AutherId = req.staffId;
+    const expenseInvoiceId = req.params.expenseInvoiceId;
+    if (!mongoose.Types.ObjectId.isValid(AutherId)) {
+      return res.status(400).json({
+        success: false,
+        error: true,
+        message: "Invalid AutherId",
+      });
+    }
+    if (!mongoose.Types.ObjectId.isValid(expenseInvoiceId)) {
+      return res.status(400).json({
+        success: false,
+        error: true,
+        message: "Invalid expenseInvoiceId",
+      });
+    }
+
+    const AutherData = await Worker.findById(AutherId);
+    if (!AutherData) {
+      return res.status(404).json({
+        success: false,
+        error: true,
+        message: "Author not found",
+      });
+    }
+    if (AutherData.role !== "Admin" && AutherData.role !== "Manager") {
+      return res.status(403).json({
+        success: false,
+        error: true,
+        message: "You are not authorized to get this expense invoice",
+      });
+    }
+    const ExpenseInvoiceData = await ExpenseInvoice.findById(expenseInvoiceId)
+      .populate("BranchId")
+      .populate("expenseInvoiceCreatedBy");
+
+    return res.status(200).json({
+      success: true,
+      error: false,
+      message: `Expense invoice fetched successfully`,
+      data: ExpenseInvoiceData,
+    });
+  } catch (error) {
+    console.log(` error in getting one expense invoice: ${error}`);
+    return res.status(500).json({
+      success: false,
+      error: true,
+      message: `Error in getting one expense invoice: ${error}`,
+    });
+  }
+};
+
+export const deleteExpenseInvoice = async (req, res) => {
+  try {
+    const AutherId = req.staffId;
+    const expenseInvoiceId = req.params.expenseInvoiceId;
+    if (!mongoose.Types.ObjectId.isValid(AutherId)) {
+      return res.status(400).json({
+        success: false,
+        error: true,
+        message: "Invalid AutherId",
+      });
+    }
+    if (!mongoose.Types.ObjectId.isValid(expenseInvoiceId)) {
+      return res.status(400).json({
+        success: false,
+        error: true,
+        message: "Invalid expenseInvoiceId",
+      });
+    }
+    const AutherData = await Worker.findById(AutherId);
+    if (!AutherData) {
+      return res.status(404).json({
+        success: false,
+        error: true,
+        message: "Author not found",
+      });
+    }
+
+    if (AutherData.role !== "Admin") {
+      return res.status(403).json({
+        success: false,
+        error: true,
+        message: "You are not authorized to delete this expense invoice",
+      });
+    }
+
+    const ExpenseInvoiceData = await ExpenseInvoice.findByIdAndDelete(
+      expenseInvoiceId
+    );
+    return res.status(200).json({
+      success: true,
+      error: false,
+      message: `Expense invoice deleted successfully`,
+      data: ExpenseInvoiceData,
+    });
+    
+  } catch (error) {
+    console.log(` error in deleting expense invoice: ${error}`);
+    return res.status(500).json({
+      success: false,
+      error: true,
+      message: `Error in deleting expense invoice: ${error}`,
     });
   }
 };

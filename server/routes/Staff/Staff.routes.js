8ced4310da4198bc5ad3685e48fbe9484837fb59
hydrazin60 <sureshkhdk45@gmail.com>
2025-02-19 12:38:41 +0545

    ##

--- server/routes/Staff/Staff.routes.js
+++ server/routes/Staff/Staff.routes.js
@@ -1,20 +1,20 @@
-// import express from "express";
-// import { isAuthenticated } from "../../middleware/isAuthenticated.js";
-// import {
-//   GetAllStaffData,
-//   GetOneStaffData,
-//   UpdateStaffData,
-// } from "../../controllers/Manager/Staff/Staff.controller.js";
-// import { uploadSingleImage } from "../../middleware/multer.js";
+import express from "express";
+import { isAuthenticated } from "../../middleware/isAuthenticated.js";
+import {
+  GetAllStaffData,
+  GetOneStaffData,
+  UpdateStaffData,
+} from "../../controllers/Manager/Staff/Staff.controller.js";
+import { uploadSingleImage } from "../../middleware/multer.js";
 
-// const StaffRouter = express.Router();
+const StaffRouter = express.Router();
 
-// StaffRouter.get("/view/staff/:staffIds", isAuthenticated, GetOneStaffData);
-// StaffRouter.get("/view/all/staff", isAuthenticated, GetAllStaffData);
-// StaffRouter.put(
-//   "/update/staff",
-//   isAuthenticated,
-//   uploadSingleImage("profilePic"),
-//   UpdateStaffData
-// );
-// export default StaffRouter;
+StaffRouter.get("/view/staff/:staffIds", isAuthenticated, GetOneStaffData);
+StaffRouter.get("/view/all/staff", isAuthenticated, GetAllStaffData);
+StaffRouter.put(
+  "/update/staff",
+  isAuthenticated,
+  uploadSingleImage("profilePic"),
+  UpdateStaffData
+);
+export default StaffRouter;
